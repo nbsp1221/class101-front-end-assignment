c8fbb7cdc85955702401bd82c39caf308577de20
retn0 <nbsp1221@gmail.com>
2020-11-20 16:45:53 +0900

    🔨 Create types.ts

---
+++ src/lib/types.ts
@@ -0,0 +1,15 @@
+export interface ProductItem {
+  id: string;
+  title: string;
+  coverImage: string;
+  price: number;
+  score: number;
+  availableCoupon?: boolean;
+}
+
+export interface Coupon {
+  type: 'rate' | 'amount';
+  title: string;
+  discountRate?: number;
+  discountValue?: number;
+}
