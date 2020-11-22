b5b6499eb4e5f4bf99f13d412ba1679cb959fcc7
retn0 <nbsp1221@gmail.com>
2020-11-22 23:39:29 +0900

    🔨 Add `checked` prop for default checked state

--- src/components/controls/Checkbox/Checkbox.tsx
+++ src/components/controls/Checkbox/Checkbox.tsx
@@ -5,16 +5,18 @@ import './Checkbox.scss';
 
 interface Props {
   readonly id: string;
+  readonly checked: boolean;
   readonly onCheck: (isChecked: boolean) => void;
 }
 
 const Checkbox: React.FunctionComponent<Props> = (props: Props) => {
   const {
     id,
+    checked,
     onCheck
   } = props;
 
-  const [isChecked, setIsChecked] = React.useState(false);
+  const [isChecked, setIsChecked] = React.useState(checked);
 
   const onChangeCheckbox = React.useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
     setIsChecked(event.target.checked);
